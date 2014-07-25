ee872bdd0027919545bff8d0e2bd7ceea8393284
qoshi <298072525@qq.com>
2014-07-25 14:35:53 +0800

    tst5

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,11 @@ if ($(window).width() <= 1280) {
 
 // Variables
     tag1       = $('.pl__all'),
-    tag2       = $('.place_your_tag_name_here'),
-    tag3       = $('.place_your_tag_name_here'),
-    tag4       = $('.place_your_tag_name_here'),
-    tag5       = $('.place_your_tag_name_here'),
-    tag6       = $('.place_your_tag_name_here');
+    tag2       = $('.Leetcode'),
+    tag3       = $('.Study'),
+    tag4       = $('.Reading'),
+    tag5       = $('.Job'),
+    tag6       = $('.Technology');
 var sidebar    = $('#sidebar'),
     container  = $('#post'),
     content    = $('#pjax'),
